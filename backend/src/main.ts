68def8464cc6375b4d36fbd93964124dbcb1d47d
ikalakhi <imranekalakhi13@gmail.com>
2023-11-25 18:35:11 +0100

    adding frontend and backend framworks and connecting them

---
+++ backend/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors(); // Enable CORS
+  await app.listen(4000);
+}
+bootstrap();
---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:4000');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+fetchData();
+reportWebVitals();
